8cbdf2418bb633cffcdf26ad335d9da94e49d9b7
n3rt46 <n3rt46@gmail.com>
2022-09-21 07:37:01 -0400

    Update diy.js

--- assets/js/diy.js
+++ assets/js/diy.js
@@ -88,9 +88,9 @@
                 },
                 {
                     'name': '1200 mAh 903052 Li-Po - 5 pcs',
-                    'amount': () => ((tracker == 5) ? 1 : 2),
-                    'cost': amount * 22.99,
-                    'costAll': () => amount * 22.99,
+                    'amount': () => ((tracker == 5) ? 1 : 2) = tempAmount,
+                    'cost': tempAmount * 22.99,
+                    'costAll': () => tempAmount * 22.99,
                     'links': '<a href="https://www.amazon.com/dp/B088YKPZ9D/">Amazon Li-Po Batteries</a>'
                 },
                 {
