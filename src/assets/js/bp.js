1167d93da691b009b14ea28a72b4aea11bbd8f86
CalliePepper <faedaunt@gmail.com>
2023-02-20 10:55:20 +1100

    Updated images on body proportions to include Nighty (also fixed some wording on owoTrack and Firmware Upload / added the video)

--- src/assets/js/bp.js
+++ src/assets/js/bp.js
@@ -2,12 +2,15 @@
     let btn = document.querySelectorAll('.bpTable summary');
     btn.forEach((header) => {
         header.addEventListener('click', function(event) {
-            console.log(event.target.id);
+            let themeCol = "_d";
+            if (document.querySelector('html').classList.contains("latte")) {
+                themeCol = "_l";
+            }
             document.querySelectorAll('.bpTable details[open]').forEach((data) => {
                 if(data.target == event.target) return;
                 data.attributes.removeNamedItem('open');
             });
-            document.querySelector('#bpImage').src = "../assets/img/"+event.target.id+".png";
+            document.querySelector('#bpImage').src = "../assets/img/"+event.target.id+themeCol+".png";
         });
     });
 })();
