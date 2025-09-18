4e013c68b3ed669c76003ee36bbc0d28d82050d8
Depact <19435553+Depact@users.noreply.github.com>
2025-09-18 19:23:15 +0200

    Smol Price Calculator - Battery Runtime
    * For smol price calculator added note about battery hours of work and widespread use in community cases
    
    * docs: PR clean-up
    
    ---------
    
    Co-authored-by: ShineBrightMeow <142170650+ShineBrightMeow@users.noreply.github.com>

--- src/smol-slimes/assets/js/smol-building-calculator.js
+++ src/smol-slimes/assets/js/smol-building-calculator.js
@@ -71,7 +71,7 @@
             name: "Batteries",
             choices: [
                 {
-                    name: "401230 3.7V 110mAh Battery",
+                    name: "401230 3.7V 110mAh Battery.<br/>Provides approximately 25 hours of runtime.<br/>Most community cases are designed to accommodate it.",
                     amount: () => tracker,
                     cost: () => 8.49 / 10,
                     costAll: () => tracker * (8.49 / 10) + 5.2,
