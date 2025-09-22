5926849c991e10a1b656a2e86d07dba27b879962
ShineBrightMeow <142170650+ShineBrightMeow@users.noreply.github.com>
2025-09-22 11:44:33 -0700

    docs: PR Clean-up

--- src/smol-slimes/assets/js/smol-building-calculator.js
+++ src/smol-slimes/assets/js/smol-building-calculator.js
@@ -276,7 +276,7 @@
                     cost: () => Math.round(tracker/7) * 6.38 + tracker * 0.36,
                     costAll: () => Math.round(tracker/7) * 6.38 + tracker * 0.36,
                     links: '\
-                    <a href=\"smol-receiver.html#option-3-wi-fi-antenna-mod" target="_blank">Depact Smol Sudo Dock docs reference.</a>\
+                    <a href=\"smol-slimes-community-builds.html#depact-smol-sudo-dock" target="_blank">Depact Smol Sudo Dock docs reference.</a>\
                     <br/>\
                     Parts:\
                     <ul>\
@@ -284,7 +284,7 @@
                             <a href="https://pl.aliexpress.com/item/1005007738886550.html" target="_blank">7 Ports USB Hub</a>\
                         </li>\
                         <li>\
-                            <a href=\"https://pl.aliexpress.com/item/4000298368244.html" target="_blank">Type C Ma to USB Male\
+                            <a href=\"https://aliexpress.com/item/1005007396270447.html" target="_blank">Type-C Male to USB-A Male OTG Connector\
                             </a>\
                         </li>\
                     </ul>',
